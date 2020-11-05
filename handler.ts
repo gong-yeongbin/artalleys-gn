bfea0456bc6e5a0026ad522221bdf9fe4b5dde04
bin <youngbin816@gmail.com>
2020-11-05 22:03:45 +0900

    typeorm

--- handler.ts
+++ handler.ts
@@ -1,7 +1,12 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import "source-map-support/register";
+import { getDatabaseConnection } from "./src/connection/Connection";
+import { User } from "./src/entity/User";
 
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
+  const connection = await getDatabaseConnection();
+  const userRepository = connection.getRepository(User);
+  console.log(userRepository);
   return {
     statusCode: 200,
     body: JSON.stringify(
---
+++ src/connection/Connection.ts
@@ -0,0 +1,80 @@
+import {
+  ConnectionOptions,
+  Connection,
+  getConnectionManager,
+  DefaultNamingStrategy,
+} from "typeorm";
+
+const CONNECTION_OPTIONS: ConnectionOptions = {
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: "root",
+  password: "1111",
+  database: "gn",
+  synchronize: true,
+  entities: ["entities/*.*"],
+};
+
+/**
+ * Establishes and returns a connection to the database server. If an existing
+ * connection is found, the connection is reused.
+ *
+ * @see https://github.com/typeorm/typeorm/issues/2598#issue-345445322
+ * @export
+ * @returns {Promise<Connection>}
+ */
+export async function getDatabaseConnection(): Promise<Connection> {
+  try {
+    console.log("Establishing connection...");
+    const connectionManager = getConnectionManager();
+    let connection: Connection;
+
+    if (connectionManager.has("default")) {
+      console.log("Reusing existion connection...");
+      connection = injectConnectionOptions(
+        connectionManager.get(),
+        CONNECTION_OPTIONS
+      );
+    } else {
+      connection = connectionManager.create(CONNECTION_OPTIONS);
+      await connection.connect();
+    }
+
+    console.log("Connection established");
+    return connection;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+}
+
+/**
+ * Injects missing / outdated connection options into an existing database
+ * connection.
+ *
+ * @see https://github.com/typeorm/typeorm/issues/2598#issue-345445322
+ * @param {Connection} connection
+ * @param {ConnectionOptions} CONNECTION_OPTIONS
+ * @returns {Connection}
+ */
+function injectConnectionOptions(
+  connection: Connection,
+  CONNECTION_OPTIONS: ConnectionOptions
+): Connection {
+  // @ts-ignore
+  connection.options = CONNECTION_OPTIONS;
+  // @ts-ignore
+  connection.manager = connection.createEntityManager();
+  // @ts-ignore
+  connection.namingStrategy =
+    connection.options.namingStrategy || new DefaultNamingStrategy();
+  // @ts-ignore
+  connection.relationLoader = new RelationLoader(connection);
+  // @ts-ignore
+  connection.relationIdLoader = new RelationIdLoader(connection);
+  // @ts-ignore
+  connection.buildMetadatas();
+
+  return connection;
+}
